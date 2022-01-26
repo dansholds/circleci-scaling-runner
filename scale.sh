95248e16c9d227b54e70f89ba844c3d3127dedb0
Dan Holdsworth <dan.holdsworth@circleci.com>
2022-01-26 09:19:09 +0000

    extracted all tokens into variables

--- scale.sh
+++ scale.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-#Gather the number of waiting tasks in the docker-resource-class
-WAITING_TASKS=$(curl -X GET "https://runner.circleci.com/api/v2/runner/tasks?resource-class=danielholdsworth/docker-resource-class" -H \
-'Circle-Token: a21996172f944cf4648fd21dc051911f6f335064' | sed 's/[^0-9]*//g')
+IMAGE_ID=5600498a75fa
+RESOURCE_CLASS_TOKEN=239586226acd7fe55233fe79812096f10f130cf242a3f47bcbe5702adf3440f8c7854076478b3dce
+CIRCLECI_TOKEN=a21996172f944cf4648fd21dc051911f6f335064
+RESOURCE_CLASS=danielholdsworth/docker-resource-class
+
 #Set initial value for the RUNNING_TASKS variable
 RUNNING_TASKS=1
+#Gather the number of waiting tasks in the docker-resource-class
+WAITING_TASKS=$(curl -X GET "https://runner.circleci.com/api/v2/runner/tasks?resource-class=$RESOURCE_CLASS" -H \
+'Circle-Token: $CIRCLECI_TOKEN' | sed 's/[^0-9]*//g')
+
 
 #if statement that is only actions if the waiting jobs are NOT 0
 if (( $WAITING_TASKS != 0 )); then
     #for loop that creates as many runner containers as needed and suffixs a number to the runner name based on how many there is
     for i in $(seq $WAITING_TASKS); do
-        CIRCLECI_RESOURCE_CLASS=danielholdsworth/docker-resource-class \
-        CIRCLECI_API_TOKEN=239586226acd7fe55233fe79812096f10f130cf242a3f47bcbe5702adf3440f8c7854076478b3dce \
-        docker run -d --rm --env CIRCLECI_API_TOKEN --env CIRCLECI_RESOURCE_CLASS --name runner-$i 5600498a75fa
+        docker run -d --rm --env CIRCLECI_TOKEN --env $RESOURCE_CLASS --name runner-$i $IMAGE_ID
     done
 
-    echo "$WAITING_TASKS containers spun-up"
+    echo "$WAITING_TASKS container(s) spun-up"
 
     #until block that polls for the amount of running jobs in the resource-class, runs until this number is 0
     until [ $RUNNING_TASKS == 0 ]; do
-        RUNNING_TASKS=$(curl -X GET "https://runner.circleci.com/api/v2/runner/tasks/running?resource-class=danielholdsworth/docker-resource-class" -H \
-        'Circle-Token: a21996172f944cf4648fd21dc051911f6f335064' | sed 's/[^0-9]*//g')
+        RUNNING_TASKS=$(curl -X GET "https://runner.circleci.com/api/v2/runner/tasks/running?resource-class=$RESOURCE_CLASS" -H \
+        'Circle-Token: $CIRCLECI_TOKEN' | sed 's/[^0-9]*//g')
     done
 
     echo "Running tasks at $RUNNING_TASKS...Shutting down containers..."
