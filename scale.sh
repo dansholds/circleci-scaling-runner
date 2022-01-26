e328a3cf0651eee3e1aae5fcc5c4884c37006b0d
Dan Holdsworth <dan.holdsworth@circleci.com>
2022-01-26 14:28:18 +0000

    reverted those changes

--- scale.sh
+++ scale.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#NOTES:
+
 #RESOURCE_CLASS= This variable is stored in the project env vars on CCI
 #CIRCLE_TOKEN= This is your CCI API Token and should be stored in CCI env vars
-
-DOCKER_IMAGE_ID=5600498a75fa
 
 #Gather the number of waiting tasks in the docker-resource-class
 WAITING_TASKS=$(curl -X GET 'https://runner.circleci.com/api/v2/runner/tasks?resource-class='$RESOURCE_CLASS'' -H \
@@ -15,7 +13,9 @@ RUNNING_TASKS=1
 if [ $WAITING_TASKS != 0 ]; then
     #for loop that creates as many runner containers as needed and suffixs a number to the runner name based on how many there is
     for i in $(seq $WAITING_TASKS); do
-        docker run -d --rm --env $DOCKER_RESOURCE_TOKEN --env $RESOURCE_CLASS --name runner-$i $DOCKER_IMAGE_ID
+        CIRCLECI_RESOURCE_CLASS=$RESOURCE_CLASS \
+        CIRCLECI_API_TOKEN=239586226acd7fe55233fe79812096f10f130cf242a3f47bcbe5702adf3440f8c7854076478b3dce \
+        docker run -d --rm --env CIRCLECI_API_TOKEN --env CIRCLECI_RESOURCE_CLASS --name runner-$i 5600498a75fa
     done
 
     echo "$WAITING_TASKS containers spun-up"
@@ -23,7 +23,7 @@ if [ $WAITING_TASKS != 0 ]; then
     #until block that polls for the amount of running jobs in the resource-class, runs until this number is 0
     until [ $RUNNING_TASKS == 0 ]; do
         RUNNING_TASKS=$(curl -X GET 'https://runner.circleci.com/api/v2/runner/tasks/running?resource-class='$RESOURCE_CLASS'' -H \
-        'Circle-Token: '$CIRCLE_TOKEN'' | sed 's/[^0-9]*//g')
+        'Circle-Token: a21996172f944cf4648fd21dc051911f6f335064' | sed 's/[^0-9]*//g')
     done
 
     echo "Running tasks at $RUNNING_TASKS...Shutting down containers..."
